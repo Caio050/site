d9448ffd7ad04fbba9f9cc834890f5bd1ff7dbea
Caio050 <caiomeiralima01@gmail.com>
2025-04-04 17:43:17 -0300

    cadastro e javascript atualizados

--- script.js
+++ script.js
@@ -1,51 +1,65 @@
 
+function validarcadastro()
+{
 
-function cadastro(){
-
-    var textousuario;
-    textousuario =document.getElementById('txtNome').value;
-    var textosobrenome =document.getElementById('txtSobrenome').value;
-    var textoEmail =document.getElementById('txtEmail').value;
+    let usuario = document.getElementById('txtnome').value.trim();
     
-    var textosenha =document.getElementById('txtSenha').value;
-    var textoConfirmar =document.getElementById('txtConfirmarsenha').value;
+    let sobrenome = document.getElementById('txtsobrenome').value.trim();
+
+    let senha = document.getElementById('txtsenha').value.trim();
+    let confisenha = document.getElementById('txtconfisenha').value.trim();
+    let datanasc = document.getElementById('txtdata').value;
+
+    if (usuario ==""){
+        alert("Por favor, preencher usuario");
+        return;
+
+    } else if (sobrenome == ""){
+        alert("Por favor, preencher o sobrenome");
+        return;
+    } else if (senha == ""){
+        alert("Por favor , preencha a senha");
+        return;
+
+    } else if (confisenha == ""){
+        alert("Por fazer, confirmar senha");
+        return;
+    }
+    if (!validarIdade(datanasc)) {
+
+        alert("Você deve ser maior de idade para se cadastrar.");
+
+        return;
+
+    }
+    function validarIdade(datanasc) {
+
+        const hoje = new Date();
     
-
-
-
-    if (textousuario == "")
-    { 
-        alert ("Preencha seu usuario");
-    }
-     else if (textosobrenome == "")
-    {
-        alert("Preencha o campo do sobrenome ");
-    }
-    else if (textoEmail == "")
-    {
-        alert ("Preencha o campo do email");
-    } 
-    else if (textosenha == "")
-    {
-        alert ("Preencha o campo da senha");
-    }
-    else if  (textoConfirmar == "")
-    {
-        alert ("Confirme sua senha");
-    }
-
-
-    else if (textousuario == "Caio" && textosenha == "123")
-    {
-        window.location.href = 'tela.html';
-    }
-    else {
-        alert ("invalido");
-    }
-
-
-
-
-
+        const nascimento = new Date(datanasc);
+    
+    
+    
+        let idade = hoje.getFullYear() - nascimento.getFullYear();
+    
+        const mes = hoje.getMonth() - nascimento.getMonth();
+    
+    
+    
+        if (mes < 0 || (mes === 0 && hoje.getDate() < nascimento.getDate())) {
+    
+            idade--;
+    
         }
+    
+    
+    
+        return idade >= 18;
+    
+    }
+
+}
+
+
+
 
